a65a4dbaccbcaface64c6b0cc0388eb79a1e96e7
raihan <76199939+raihanrivanaputra@users.noreply.github.com>
2020-12-18 14:31:27 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,117 @@
+<html>
+    <head>
+        <title>Data Pendidikan & Tenaga kependidikan</title>
+        <link rel="stylesheet" href="style/style-home.css">
+
+    </head>
+    <body>
+      
+	<style type="text/css">
+        body {
+                    background-image:url(https://www.ilmubahasa.net/wp-content/uploads/2018/10/backgroundkeren1-630x380.jpg);
+                    background-size: cover;
+                
+                }
+        #box {
+        width:1290px;
+                    height:100x;
+                    background:url(https://www.ilmubahasa.net/wp-content/uploads/2018/10/backgroundkeren1-630x380.jpg);
+                    box-shadow: 0px 0px 10px 5px darkblue;
+                    transform: translate(-50px -50px);				
+                    margin:0px 28px 0px 40px ;
+                    font-size: xx-large; 
+                    text-align: center;
+                    color : wheat ;
+                    font-family: fantasy;
+                    border: 4px solid ;
+         border-radius: 10px;
+        }
+        html,body{
+            padding: 0;
+            margin:0;
+            font-family: sans-serif;
+        
+        }
+    
+        .menu-Home{
+            background-color: rgba(0,0,0,0.2);
+            border: 4px solid darkblue;
+         border-radius: 10px;
+            margin:0px 30px 0px 30px ;
+        }
+    
+        .menu-Home ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+    
+        .menu-Home > ul > li {
+            float: left;
+        }
+    
+        
+        .menu-Home li a {
+            display: inline-block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+    
+        .menu-Home li a:hover{
+            background-color: white;
+            color: black ;
+        }
+    
+        li.dropdown {
+            display: inline-block;
+        }
+    
+        .dropdown:hover .isi-dropdown {
+            display: block;
+        }
+    
+        .isi-dropdown a:hover {
+            color: #fff !important;
+        }
+    
+        .isi-dropdown {
+            position: absolute;
+            display: none;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+            background-color: #f9f9f9;
+        }
+    
+        .isi-dropdown a {
+            color: #3c3c3c !important;
+        }
+    
+        .isi-dropdown a:hover {
+            color: #232323 !important;
+            background: #f3f3f3 !important;
+        }
+    </style>
+    
+    
+    <header class="header">
+        <div id="box"><br>Data Pendidikan & Tenaga kependidikan<br></be><br></div>
+            <div class="menu-home">
+    
+            <ul>
+                <li><a href="home.php">home</a></li>
+                <li><a href="about.php">about</a></li>
+                <li class="dropdown"><a href="pendidik.php">pendidik dan kependidikan</a>
+                    <ul class="isi-dropdown">
+                        <li><a href="atasan.php"><bg color ="orangered"> kepala dan wakil sekolah</bg></a></li>
+                        <li><a href="guru.php"><bg color ="orangered"></bg>guru</a></li>
+                            
+                    </ul>
+                </li>
+            </ul>
+    
+        </div>
+    </body>
+</html>
